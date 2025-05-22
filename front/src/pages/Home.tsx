e2a495b999ebb709fc309d348cc88c245ddb96ad
JoseDeyvid <j_deyvid@live.com>
2025-05-22 10:10:34 -0300

    Adicionar biblioteca do mercado pago e react no front e testar o exemplo padrao que eles fornecem

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,18 +1,46 @@
-import Card from '../components/Card';
-import { useProducts } from '../contexts/ProductsContext';
+// import Card from '../components/Card';
+// import { useProducts } from '../contexts/ProductsContext';
+
+
+// const Home = () => {
+//     const { products, setProducts } = useProducts();
+//     return (
+//         <div>
+//             <h1>Produtos</h1>
+//             <ul>
+//                 {products.map((product) => (
+//                     <Card key={product.id} product={product} products={products} setProducts={setProducts} />
+//                 ))}
+//             </ul>
+//         </div>
+//     )
+// }
+
+// export default Home
+
+import { initMercadoPago, CardPayment, } from '@mercadopago/sdk-react';
+import type { ICardPaymentFormData, ICardPaymentBrickPayer } from '@mercadopago/sdk-react/esm/bricks/cardPayment/type';
+import axios from 'axios';
+initMercadoPago(import.meta.env.VITE_PUBLIC_KEY);
 
 const Home = () => {
-    const { products, setProducts } = useProducts();
+
+    const handlePayment = async (param: ICardPaymentFormData<ICardPaymentBrickPayer>) => {
+        console.log("Chegando aqui!")
+        try {
+            await axios.post("http://localhost:3000/credit", param)
+        } catch (error) {
+            console.log("Erro: ", error)
+        }
+
+    }
+
     return (
-        <div>
-            <h1>Produtos</h1>
-            <ul>
-                {products.map((product) => (
-                    <Card key={product.id} product={product} products={products} setProducts={setProducts} />
-                ))}
-            </ul>
-        </div>
-    )
+        <CardPayment
+            initialization={{ amount: 2000 }}
+            onSubmit={async (param) => handlePayment(param)}
+        />
+    );
 }
 
-export default Home
+export default Home;
